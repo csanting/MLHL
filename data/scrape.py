5665345389cbf9c9995f29971593274171a8d089
csanting <codysanting@hotmail.com>
2025-03-10 14:32:27 -0700

    Update scrape scipt to gather playoff data. Add Skater Playoff Data csv

--- data/scrape.py
+++ data/scrape.py
@@ -8,6 +8,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
+REGULAR_SEASON_TYPE = 2
+PLAYOFF_SEASON_TYPE = 3
+
+# URL of the NHL stats page
+URL_TEMPLATE = 'https://www.nhl.com/stats/skaters?aggregate=0&reportType=season&seasonFrom=20052006&seasonTo=20232024&gameType={}&status=active&sort=points,goals,assists&page={}&pageSize=100'
+
+NUM_PAGES = 58
+NUM_PLAYOFF_PAGES = 25
+
 # Function to scrape data from a single page with WebDriverWait
 def scrape_page_data(driver):
     # Locate the table element
@@ -22,46 +31,48 @@ def scrape_page_data(driver):
     
     return data
 
-# Initialize the WebDriver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def collect_data(driver, fileName, pages, type):
+    all_data = []
 
-# URL of the NHL stats page
-url_template = 'https://www.nhl.com/stats/skaters?aggregate=0&reportType=season&seasonFrom=20052006&seasonTo=20232024&gameType=2&status=active&sort=points,goals,assists&page={}&pageSize=100'
+    # Loop through all pages
+    for page in range(0, pages+1):
+        # Load the webpage
+        driver.get(URL_TEMPLATE.format(type, page))
+        
+        # Wait for the page to load
+        time.sleep(5)  # Increase or decrease sleep depending on your internet speed
+        
+        # Scrape data from the current page
+        page_data = scrape_page_data(driver)
+        print(page)
+        
+        # Append the data to the full dataset
+        all_data.extend(page_data)
 
-all_data = []
-numPages = 56
+    # Define the maximum number of columns
+    max_columns = max(len(row) for row in all_data)
 
-# Loop through all pages (0 to 56)
-for page in range(0, numPages+1):
-    # Load the webpage
-    driver.get(url_template.format(page))
+    # Pad rows that have fewer columns
+    padded_data = [row + [None] * (max_columns - len(row)) for row in all_data]
+
+    # Convert the padded data into a NumPy array
+    data_np = np.array(padded_data)
+
+    # Now create the DataFrame
+    columns = ['Rank', 'Player', 'Season', 'Team', 'Skater Shoots', 'Player Position', 'Games Played', 'Goals', 'Assists', 'Points', 'Plus-Minus', 'Penalty in Minutes', 'Points Per Game Played', 'Even Strength Goals', 'Even Strength Points', 'Power Play Goals', 'Power Play Points', 'Shorthanded Goals', 'Shorthanded Points', 'Overtime Goals', 'Game Winning Goals', 'Shots', 'Shooting Percentage', 'Time on Ice per Game Played', 'Face-off Win Percentage']
+
+    df = pd.DataFrame(data_np, columns=columns)
+
+    # Save to CSV
+    df.to_csv(fileName, index=False)
+
+if __name__ == "__main__":
+    # Initialize the WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    # Collect Regular Season and Playoff data in separate files
+    collect_data(driver, 'skater_stats.csv', NUM_PAGES, REGULAR_SEASON_TYPE)
+    collect_data(driver, 'skater_playoff_stats.csv', NUM_PLAYOFF_PAGES, PLAYOFF_SEASON_TYPE)
     
-    # Wait for the page to load
-    time.sleep(5)  # Increase or decrease sleep depending on your internet speed
-    
-    # Scrape data from the current page
-    page_data = scrape_page_data(driver)
-    print(page)
-    
-    # Append the data to the full dataset
-    all_data.extend(page_data)
-
-# Define the maximum number of columns
-max_columns = max(len(row) for row in all_data)
-
-# Pad rows that have fewer columns
-padded_data = [row + [None] * (max_columns - len(row)) for row in all_data]
-
-# Convert the padded data into a NumPy array
-data_np = np.array(padded_data)
-
-# Now create the DataFrame
-columns = ['Rank', 'Season', 'Player', 'Team', 'Skater Shoots', 'Player Position', 'Games Played', 'Goals', 'Assists', 'Points', 'Plus-Minus', 'Penalty in Minutes', 'Points Per Game Played', 'Even Strength Goals', 'Even Strength Points', 'Power Play Goals', 'Power Play Points', 'Shorthanded Goals', 'Shorthanded Points', 'Overtime Goals', 'Game Winning Goals', 'Shots', 'Shooting Percentage', 'Time on Ice per Game Played', 'Face-off Win Percentage']
-
-df = pd.DataFrame(data_np, columns=columns)
-
-# Save to CSV
-df.to_csv('skater_stats.csv', index=False)
-
-# Close the driver
-driver.quit()
+    # Close the driver
+    driver.quit()
